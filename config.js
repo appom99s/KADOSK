08b7f8b10d1c03bdeef38007c9faa3d630d2529b
appom99s <63603223+appom99s@users.noreply.github.com>
2026-08-15 22:12:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.KADOSK_CONFIG = {
-  clientId: "42596ab1-7d24-468b-8a31-abd953f52194"
+  clientId: "42596ab1-7d24-468b-8a31-abd953f52194",
   siteBaseUrl: "https://www.kadosk.com",
   frontendBaseUrl: "https://merchant.kadosk.com",
   loginCallbackPath: "/login-callback.html",
