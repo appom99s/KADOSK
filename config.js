06a1fc87bcf584f8a0f0a9b4625440412afcb9e3
appom99s <63603223+appom99s@users.noreply.github.com>
2026-08-15 21:56:42 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.KADOSK_CONFIG = {
-  clientId: "42596ab1-7d24-468b-8a31-abd953f52194,
-  siteBaseUrl: "https://www.kadosk.com",
-  frontendBaseUrl: "https://merchant.kadosk.com",
-  loginCallbackPath: "/login-callback.html",
-  logoutRedirectPath: "/login.html"
-};
-///
